e3aa8ef47e1d75c011c28dfd866b861dc28337e4
mk-anderson11 <matt.anderson11@gmail.com>
2016-11-29 10:41:43 -0700

    Working Oxygen Balance

--- traegerMassBalance.py
+++ traegerMassBalance.py
@@ -6,13 +6,15 @@ import csv
 def traeger(X, t, fanSpeed):
     XO2 = X # Oxygen mass in grill [lb]
 
-    rO2 = 0.1         # Rate of reaction/combustion O2 [lb/hr]
-    m_air = 5         # Flowrate of air (flowrate in and out through flue) [lb/hr]
+    rO2 = 1.0         # Rate of reaction/combustion O2 [lb/hr]
+    m_air = 5.         # Flowrate of air (flowrate in and out through flue) [lb/hr]
     X_O2_in = 0.21    # Mass fraction of O2 in air
-    m_grill = 10      # Mass of air in grill [lb]
+    m_grill = 10.      # Mass of air in grill [lb]
 
     # Calculate the dXO2dt
     dXO2dt = (1/m_grill) * (m_air * (X_O2_in - XO2) - rO2)
+    # print("XO2 = ", XO2)
+    # print(dXO2dt)
 
     return dXO2dt
 
@@ -20,8 +22,10 @@ def traeger(X, t, fanSpeed):
 XO2_0 = 0.5   # K
 
 # Time points for solution
-tf = 200 # s
+tf = 10 # hr
+nsteps = 11
 t = np.linspace(0, tf, tf+1)
+delta_t = tf/(nsteps - 1)
 
 # Fan speed can be adjusted
 fan = np.ones((tf+1))
@@ -32,18 +36,24 @@ fan[150:] = 5
 
 # Record solution
 y = np.empty((tf+1))
-y[0] = T0
+y[0] = XO2_0
 
 # Simulate the grill step test
 for i in range(tf):
     # Specify fan speed
     inputs = (fan[i],)
     # Integrate the model
-    XO2 = odeint(traeger, T0, [0,1], inputs)
+
+    print("XO2_0", XO2_0)
+
+    XO2 = odeint(traeger, XO2_0, [0,delta_t], inputs)
+
+    print("XO2 = ", XO2[-1][0])
+
     # Record result
     y[i+1] = XO2[-1]
     # Reset initial conditions
-    T0 = XO2[-1]
+    XO2_0 = XO2[-1]
 
 # Plot results
 plt.figure(1)
